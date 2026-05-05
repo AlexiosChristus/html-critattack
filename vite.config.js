7983cab258d883a37164ed1d2e154fd6c2d52866
Cristea Alexandru <speedgun6@gmail.com>
2026-05-05 16:13:19 +0300

    New vite config test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,34 @@
-// vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
+import fg from "fast-glob";
+
+// автоматически находим все html файлы
+function getHtmlEntries() {
+  const files = fg.sync(["*.html", "posts/**/*.html"]);
+
+  const entries = {};
+
+  files.forEach((file) => {
+    const name = file.replace(".html", "");
+    entries[name] = resolve(__dirname, file);
+  });
+
+  return entries;
+}
 
 export default defineConfig({
   base: "./",
+
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "partials"),
     }),
   ],
+
+  build: {
+    rollupOptions: {
+      input: getHtmlEntries(),
+    },
+  },
 });
